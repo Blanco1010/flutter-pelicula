de87f6c55f5c64a92de3c218c406018296ae848c
Juan Camilo Blanco Martinez <camilo3198@gmail.com>
2021-08-21 17:36:10 -0500

    add Swiper

--- peliculas/lib/src/widgets/card_swiper.dart
+++ peliculas/lib/src/widgets/card_swiper.dart
@@ -1,13 +1,48 @@
+import 'dart:html';
+
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 
 class CardSwiper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Container(
+      padding: EdgeInsets.only(top: size.height * 0.05),
       width: double.infinity,
-      height: 500,
-      color: Colors.red,
-      child: null,
+      height: size.height * 0.5,
+      child: Swiper(
+        itemCount: 10,
+        layout: SwiperLayout.STACK,
+        itemWidth: size.width * 0.6,
+        itemHeight: size.height * 0.9,
+        itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(context, _createRoute());
+            },
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: FadeInImage(
+                placeholder: AssetImage('assets/gifs/loading.gif'),
+                image: NetworkImage('https://via.placeholder.com/300x400'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          );
+        },
+      ),
     );
+  }
+
+  _createRoute() {
+
+    return PageRouteBuilder(pageBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation){})
+
+
+
+
+
   }
 }
