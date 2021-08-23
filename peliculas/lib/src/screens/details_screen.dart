ab67ac025b98f337a6f467f5635a2dbe3c56993c
Juan Camilo Blanco Martinez <camilo3198@gmail.com>
2021-08-22 19:58:29 -0500

    add title, subtitle and vote acerage

--- peliculas/lib/src/screens/details_screen.dart
+++ peliculas/lib/src/screens/details_screen.dart
@@ -13,6 +13,11 @@ class DetailsScreen extends StatelessWidget {
         physics: BouncingScrollPhysics(),
         slivers: [
           _CustomAppBar(),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              _PosterAndTitle(),
+            ]),
+          ),
         ],
       ),
     );
@@ -44,6 +49,64 @@ class _CustomAppBar extends StatelessWidget {
           image: NetworkImage('https://via.placeholder.com/500x300'),
           fit: BoxFit.cover,
         ),
+      ),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme theme = Theme.of(context).textTheme;
+
+    final Size size = MediaQuery.of(context).size;
+
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.all(Radius.circular(30)),
+            child: FadeInImage(
+              placeholder: AssetImage('assets/img/no-image.jpg'),
+              image: NetworkImage('https://via.placeholder.com/200x300'),
+              height: size.height * 0.3,
+            ),
+          ),
+          SizedBox(width: size.width * 0.02),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'movie.title',
+                style: theme.headline5,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              Text(
+                'movie.origialTitle',
+                style: theme.subtitle2,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.star_outline,
+                    size: size.height * 0.03,
+                    color: Colors.grey,
+                  ),
+                  SizedBox(height: size.height * 0.05),
+                  Text(
+                    'movie.voteAverage',
+                    style: theme.caption,
+                  )
+                ],
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
